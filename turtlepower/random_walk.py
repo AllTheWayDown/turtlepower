8960e6fa1dd70ce57770ddafba2797fa889a684f
Simon Davy <bloodearnest@gmail.com>
2013-09-25 22:40:03 +0100

    make random_walk look more like turgles demo

--- turtlepower/random_walk.py
+++ turtlepower/random_walk.py
@@ -1,15 +1,15 @@
 import sys
-from random import random
+from random import random, randint
 
 from turtlepower.world import TurtleWorld
 
 # simulation parameters
-W = 800
-H = 800
-n = 100  # num turtles
+W = 600
+H = 600
+num_turtles = 100  # num turtles
 
 if len(sys.argv) > 1:
-    n = int(sys.argv[1])
+    num_turtles = int(sys.argv[1])
 
 max_speed = 2  # how fast they move
 max_turn = 10  # maximum turn speed in degrees
@@ -23,8 +23,22 @@ def random_walk(turtle, world=None):
     turtle.right(angle)
     turtle.forward(max_speed)
 
+shapes = [
+    'turtle',
+    'classic',
+    'square',
+    'circle',
+    'triangle',
+    'arrow',
+]
 
-for _ in range(n):
-    world.create_turtle(callback=random_walk)
+n = num_turtles // len(shapes)
+
+for shape in shapes:
+    for _ in range(n):
+        turtle = world.create_turtle(callback=random_walk)
+        turtle.penup()
+        turtle.shape(shape)
+        turtle.color(random(), random(), random())
 
 world.run()
